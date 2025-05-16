e2ad919021ecf32d345739a05971344658b7725b
Rayane <rayane.gueddouche@epitech.eu>
2025-05-16 11:08:04 +0200

    hook usematch

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,28 @@
 import { NavLink, useMatch } from "react-router";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import "./Header.css";
 
 const Header = () => {
   const match = useMatch("/categorie/:category");
 
   return (
-    <Navbar bg="light" expand="lg" className="justify-content-end border-bottom">
-      <Container className="justify-content-end">
-        <Navbar.Collapse id="main-navbar" className="justify-content-end">
-          <Nav>
-            <Nav.Link as={NavLink} to="/" end>Accueil</Nav.Link>
-            <Nav.Link as={NavLink} to="/categorie/vetements">Vêtements</Nav.Link>
-            <Nav.Link as={NavLink} to="/categorie/chaussures">Chaussures</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+    <Navbar bg="light" expand="lg" className="border-bottom py-3">
+      <Container className="d-flex justify-content-between align-items-center">
+        <div className="fw-bold">
+          {match && `Catégorie : ${match.params.category}`}
+        </div>
+
+        <Nav className="ms-auto">
+          <Nav.Link as={NavLink} to="/" end>
+            Accueil
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/categorie/vetements">
+            Vêtements
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/categorie/chaussures">
+            Chaussures
+          </Nav.Link>
+        </Nav>
       </Container>
     </Navbar>
   );
